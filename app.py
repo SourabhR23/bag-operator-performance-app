b52cce9a07210a70c173dc86145e73f5b373b7ab
Sourabh Rasal <rasal.sourabh23@gmail.com>
2021-08-23 18:28:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.set_page_config(
 )
 
 # WebApp Style
-with open('static/style.css') as f:
+with open('static/style2.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 
